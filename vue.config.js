b75255615b26d9b2be5b95d0dd4d687dcda7d536
webdq <d312697510@126.com>
2020-05-08 15:59:54 +0800

    添加vue子应用

---
+++ vue.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const { name } = require('./package');
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+module.exports = {
+  outputDir: 'dist',
+  assetsDir: 'static',
+  filenameHashing: true,
+  devServer: {
+    // host: '0.0.0.0',
+    hot: true,
+    disableHostCheck: true,
+    port: 7100,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+  // 自定义webpack配置
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src'),
+      },
+    },
+    output: {
+      // 把子应用打包成 umd 库格式
+      library: `${name}-[name]`,
+      libraryTarget: 'umd',
+      jsonpFunction: `webpackJsonp_${name}`,
+    },
+  },
+};
